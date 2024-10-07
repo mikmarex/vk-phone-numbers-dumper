1869135cf9b1f6306687f96a47ac131acd554977
mikmarex <kultura.irkt@gmail.com>
2024-10-08 03:07:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Ваш токен доступа ВК
-TOKEN = ''
+TOKEN = 'N3AKHv9_KrIn5zfLVHZVvOiL1tveu7tmYS1qneev_R0G7HkXM46dxRSA1Vcrx1GNM30Cg9ueU7l50G2JywV-psb1wlW2vWXU9HON-qrtgbAB8sPhzPP9V3fquHE-b97AvCd-iQCm0-EoqMDY167voMc-BM2t0bq0L4my_QpVs5n3VXI5bLvP_SHBCcf3ZYKzrlsXolvod_pC-2zoc6T3uQ'
 
 # Версия ВК API
-VERSION = 5.92
+VERSION = 5.199
 
 # Директория, где будут храниться номера телефонов
 DUMP_DIRECTORY = 'dumps'
